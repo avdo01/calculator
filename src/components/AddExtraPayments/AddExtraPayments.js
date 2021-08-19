6a518a8d75e94bc366cd46e5229e247e4a6bb775
Kerim Balic <balickerim@gmail.com>
2021-08-19 15:43:10 +0200

    changed icons and added new component (AddExtraPayments) with responsive design

---
+++ src/components/AddExtraPayments/AddExtraPayments.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import styles from './AddExtraPayments.module.css';
+import plus from '../photos/plus.png';
+import minus from '../photos/minus.png';
+
+const AddExtraPayments = () => {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const years = [2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034];
+    const [isOpen, setIsOpen] = useState(false);
+    const handleClick = () => {
+        setIsOpen(!isOpen);
+    }
+
+    return (
+        <div className={styles.mainWrapper}>
+            <div className={styles.up}>
+                <div className={styles.leftContent}>
+                    {
+                        !isOpen ?
+                            <div className={styles.leftContentText}>
+                                Add Extra Payments
+                            </div>
+                            :
+                            <div className={styles.leftContentText}>
+                                Hide Extra Payments
+                            </div>
+                    }
+                </div>
+                <div className={styles.rightContent}>
+                    {!isOpen ? <a onClick={() => handleClick()}><img className={styles.plus} src={plus} alt='plus-logo' /></a> : null}
+                    {isOpen ? <a onClick={() => handleClick()}><img className={styles.minus} src={minus} alt='minus-logo' /></a> : null}
+                </div>
+            </div>
+            {isOpen ?
+                <div className={styles.down}>
+                    <div className={styles.downText}>
+                        <div className={styles.downTextContent}>
+                            Calculate the impact of extra payments using any combination of the
+                        </div>
+                        <div className={styles.downTextContent}>
+                            inputs below. To see your new estimated payoff date, click ‘Show
+                        </div>
+                        <div className={styles.downTextContent}>
+                            amortization schedule’ link above.
+                        </div>
+                    </div>
+                    <div className={styles.downInput}>
+                        <div className={styles.inputText}>
+                            Amount to your monthly payment
+                        </div>
+                        <div className={styles.inputs}>
+                            <input type='number' placeholder='$'></input>
+                        </div>
+                        <div className={styles.inputText}>
+                            Amount as an extra yearly payment occurring every:
+                        </div>
+                        <div className={styles.inputs}>
+                            <input type='number' placeholder='$'></input>
+                            <select>
+                                {
+                                    months.map(month => {
+                                        return (
+                                            <option>{month}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
+                        <div className={styles.inputText}>
+                            Amount as a one-time payment in:
+                        </div>
+                        <div className={styles.inputs}>
+                            <input type='number' placeholder='$'></input>
+                            <select>
+                                {
+                                    months.map(month => {
+                                        return (
+                                            <option>{month}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                            <select style={{ width: '80px' }}>
+                                {
+                                    years.map(year => {
+                                        return (
+                                            <option>{year}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        </div>
+                    </div>
+                    <div className={styles.downButton}>
+                        <button>Apply Extra Payments</button>
+                    </div>
+                </div> : null
+            }
+        </div>
+    )
+}
+
+export default AddExtraPayments;
--- src/components/AmortizationCalculator/AmortizationCalculator.js
+++ src/components/AmortizationCalculator/AmortizationCalculator.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import logo from '../photos/coolicon.png';
+import AddExtraPayments from '../AddExtraPayments/AddExtraPayments';
+import logo from '../photos/coolicon1.png';
 import './amortizationCalculator.css';
 
 const AmortizationCalculator = () => {
@@ -43,73 +44,76 @@ const AmortizationCalculator = () => {
     }
 
     return (
-        <div className='main-wrapper-am'>
-            <div className='left-content-am'>
-                <div className='left-am-input'>
-                    <div className='inputs'>
-                        <h2>Loan amount</h2>
-                        <input type='number' value={loanAmount} onChange={(e) => setLoanAmount(parseFloat(e.target.value))} />
-                        {loanAmount < 1000 || loanAmount > 999999 ? <div className='invalid'>Minimum value: $1000<br />Maximum value: $999999</div> : null}
+        <div className='main'>
+            <div className='main-wrapper-am'>
+                <div className='left-content-am'>
+                    <div className='left-am-input'>
+                        <div className='inputs'>
+                            <h2>Loan amount</h2>
+                            <input type='number' value={loanAmount} onChange={(e) => setLoanAmount(parseFloat(e.target.value))} />
+                            {loanAmount < 1000 || loanAmount > 999999 ? <div className='invalid'>Minimum value: $1000<br />Maximum value: $999999</div> : null}
+                        </div>
+                        <div className='inputs'>
+                            <h2>Loan term in years</h2>
+                            <input type='number' value={loanYears} onChange={(e) => setLoanYears(parseFloat(e.target.value))} />
+                            {loanYears > 40 ? <div className='invalid'>Maximum years: 40</div> : null}
+                        </div>
                     </div>
-                    <div className='inputs'>
-                        <h2>Loan term in years</h2>
-                        <input type='number' value={loanYears} onChange={(e) => setLoanYears(parseFloat(e.target.value))} />
-                        {loanYears > 40 ? <div className='invalid'>Maximum years: 40</div> : null}
+                    <div className='left-am-line'>
+                        <div className='line-one' />
+                        or
+                        <div className='line-one' />
                     </div>
-                </div>
-                <div className='left-am-line'>
-                    <div className='line-one' />
-                    or
-                    <div className='line-one' />
-                </div>
-                <div className='left-am-input'>
-                    <div className='inputs'>
-                        <h2>Loan term in months</h2>
-                        <input type='number' value={loanMonths} onChange={(e) => setLoanMonths(parseFloat(e.target.value))} />
-                        {loanMonths > 480 ? <div className='invalid'>Maximum months: 480</div> : null}
+                    <div className='left-am-input'>
+                        <div className='inputs'>
+                            <h2>Loan term in months</h2>
+                            <input type='number' value={loanMonths} onChange={(e) => setLoanMonths(parseFloat(e.target.value))} />
+                            {loanMonths > 480 ? <div className='invalid'>Maximum months: 480</div> : null}
 
+                        </div>
+                        <div className='inputs'>
+                            <h2>Interest rate per year</h2>
+                            <input type='number' value={interestRate} onChange={(e) => setInterestRate(parseFloat(e.target.value))} />
+                            {interestRate > 99 || interestRate < 1 ? <div className='invalid'>Minimum rate: 1<br />Maximum rate: 99</div> : null}
+                        </div>
                     </div>
-                    <div className='inputs'>
-                        <h2>Interest rate per year</h2>
-                        <input type='number' value={interestRate} onChange={(e) => setInterestRate(parseFloat(e.target.value))} />
-                        {interestRate > 99 || interestRate < 1 ? <div className='invalid'>Minimum rate: 1<br />Maximum rate: 99</div> : null}
+                    <div className='left-am-button'>
+                        <button disabled={disabled} onClick={() => handleClick()}>Calculate</button>
+                        <h2>Show amortization schedule</h2>
+                        <img src={logo} alt='calendar-logo' />
                     </div>
                 </div>
-                <div className='left-am-button'>
-                    <button disabled={disabled} onClick={() => handleClick()}>Calculate</button>
-                    <h2>Show amortization schedule</h2>
-                    <img src={logo} alt='calendar-logo' />
+                <div className='right-content-am'>
+                    <div className='right-content-first'>
+                        <div className='right-number-am'>
+                            ${payment === 0 ? 0 : payment.toFixed(2)}
+                        </div>
+                        <div className='right-text-am'>
+                            Monthly Payments
+                        </div>
+                    </div>
+                    <div className='right-content-second'>
+                        <div className='right-totals'>
+                            <div className='right-totals-text'>
+                                Total Principal Paid
+                            </div>
+                            <div className='right-totals-number'>
+                                ${loanAmount ? loanAmount : 0}
+                            </div>
+                        </div>
+                        <div className='right-totals-line' />
+                        <div className='right-totals'>
+                            <div className='right-totals-text'>
+                                Total Interest Paid
+                            </div>
+                            <div className='right-totals-number' style={{ paddingLeft: '8px' }}>
+                                ${totalInterest ? totalInterest.toFixed(2) : 0}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className='right-content-am'>
-                <div className='right-content-first'>
-                    <div className='right-number-am'>
-                        ${payment === 0 ? 0 : payment.toFixed(2)}
-                    </div>
-                    <div className='right-text-am'>
-                        Monthly Payments
-                    </div>
-                </div>
-                <div className='right-content-second'>
-                    <div className='right-totals'>
-                        <div className='right-totals-text'>
-                            Total Principal Paid
-                        </div>
-                        <div className='right-totals-number'>
-                            ${loanAmount ? loanAmount : 0}
-                        </div>
-                    </div>
-                    <div className='right-totals-line' />
-                    <div className='right-totals'>
-                        <div className='right-totals-text'>
-                            Total Interest Paid
-                        </div>
-                        <div className='right-totals-number' style={{ paddingLeft: '8px' }}>
-                            ${totalInterest ? totalInterest.toFixed(2) : 0}
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <AddExtraPayments />
         </div>
     )
 }
