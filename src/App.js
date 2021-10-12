7350fb6462225dc4abe0800cb91bad7d265d6c9a
Kerim Balic <balickerim@gmail.com>
2021-10-12 10:13:44 +0200

    added guide for building in wp app

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
-import AmortizationCalculator from './components/AmortizationCalculator/AmortizationCalculator';
-import MortgageCalculator from './components/MortgageCalculator/MortgageCalculator';
+import React from "react";
+import {
+  Route,
+  HashRouter,
+} from "react-router-dom";
+import "./App.css";
+import AmortizationCalculator from "./components/AmortizationCalculator/AmortizationCalculator";
+import MortgageCalculator from "./components/MortgageCalculator/MortgageCalculator";
 
+/**
+ *
+ * STEPS
+ *
+ * - Add the following line in the hash router (should be the ONLY child)
+ * <Route exact path="/" component={MortgageCalculator} />
+ * - build the app
+ * - upload the build folder through ftp to the io app in wp-content/reactpress/apps/io
+ * - replace the component in the hash router with the following line
+ * <Route exact path="/" component={AmortizationCalculator} />
+ * - build the app
+ * - upload the build folder through ftp to the am app in wp-content/reactpress/apps/am
+ */
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path='/nerminh18.sg-host.com/calculators-io/' component={MortgageCalculator} />
-          <Route exact path='/nerminh18.sg-host.com/calculators-am/' component={AmortizationCalculator} />
-        </Switch>
-      </Router>
+      <HashRouter>
+        <Route exact path="/" component={AmortizationCalculator} />
+      </HashRouter>
     </div>
   );
 }
